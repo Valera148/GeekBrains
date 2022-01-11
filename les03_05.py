d0c3233568aa392e9b06ee52a534b90a2284b650
Lera Andreeva <valeraandreeva148@icloud.com>
2022-01-11 16:16:53 +0300

    четвертое задание

---
+++ les03_05.py
@@ -0,0 +1,15 @@
+def my_func():
+    s = 0
+    while True:
+        list = (input('Введите числа через запятую: ')).split()
+        for n in list:
+            if n.lower() == 'q':
+                return s
+            else:
+                try:
+                    s += int(n)
+                except ValueError:
+                    print('Если хотите выйти, введите "q"')
+    print('Сумма введенных чисел равна: {s}')
+
+print(my_func())
