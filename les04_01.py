7f246489be782e4d0d53d38fbc8e8e1e022ff401
Lera Andreeva <valeraandreeva148@icloud.com>
2022-01-27 18:01:12 +0300

    первый урок, 4 задание

---
+++ les04_01.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+def salary():
+    try:
+        time, rate, bonus = map(float, argv[1:])
+        print(f'salary - {time*rate+bonus}')
+    except ValueError:
+        print('Err')
+salary()
